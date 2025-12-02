9ebe8f447704dbe2821e5e979134d8a1bbe0ea30
ghj <heejin8273@gmail.com>
2025-12-03 08:10:08 +0900

    add test dag

---
+++ dags/dags_test.py
@@ -0,0 +1,22 @@
+from airflow import DAG, BashOperator
+import pendulum
+
+
+with DAG (
+    dag_id="dags_test",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2025,1,1,tz="Asia/Seoul"),
+    catchup=False,
+    tags=["test", "bash"],
+) as dag:
+    
+    run_1 = BashOperator(
+        task_id="run_1",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE"
+    )
+    run_2 = BashOperator(
+        task_id="run_2",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh strawberry"
+    )
+
+    run_1 >> run_2
