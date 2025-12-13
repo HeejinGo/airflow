512fe5f630ce9d01661ea31db59ab154c32b3c90
ghj <heejin8273@gmail.com>
2025-12-13 19:00:50 +0900

    modify dags

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -2,9 +2,6 @@ import datetime
 import pendulum
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.bash import BashOperator #오퍼레이터 : 설계도
-import os
-
-
 
 with DAG (
     dag_id="dags_bash_with_template",
@@ -13,18 +10,19 @@ with DAG (
     catchup=False,
     tags=["test", "bash", "template"],
 ) as dag:
-    bash_t1 = BashOperator( 
-        task_id="bash_t1",
-        bash_command='echo "data_interval_start : {{ data_interval_start }}"',
+    bash_t1 = BashOperator(
+        task_id='bash_t1',
+        bash_command='echo "date_interval_start : {{ date_interval_start }}"'
     )
-    bash_t2 = BashOperator( 
-        task_id="bash_t2",
+    
+    bash_t2 = BashOperator(
+        task_id='bash_t2',
         env={
-            # 'start_date':'{{data_interval_start | ds}}',
-            'start_date':'{{ macros.ds_add(data_interval_start | ds, -1) }}',
-            'end_date':'{{data_interval_end | ds}}'
+            'start_date':'{{date_interval_start | ds}}',
+            'end_date':'{{date_interval_end | ds}}'
         },
-        bash_command='echo $start_date && echo $end_date',
+        bash_command='echo $start_date && echo $end_date'
     )
-
+    
     bash_t1 >> bash_t2
+    
