29cb4bc4a72737a846b6de82bb15d9c41fd2af1e
ghj <heejin8273@gmail.com>
2025-12-13 19:39:16 +0900

    modify dags

---
+++ dags/dags_python_template.py
@@ -0,0 +1,41 @@
+import pendulum
+
+from airflow.providers.standard.operators.python import (
+    PythonOperator,
+)
+from airflow.sdk import DAG
+from utils.get_fruit import select_fruit
+from airflow.decorators import task
+
+with DAG (
+    dag_id="dags_python_template",
+    schedule="0 1 * * *",
+    start_date=pendulum.datetime(2025,1,1,tz="Asia/Seoul"),
+    catchup=False,
+    tags=["test", "python"],
+) as dag:
+    
+    def python_func1(start_date, end_date, **kwargs):
+        print(start_date)
+        print(end_date)
+        
+    python_t1 = PythonOperator(
+        task_id='python_t1',
+        python_callable=python_func1,
+        op_kwargs={
+            'start_date':'{{data_interval_start}}',
+            'end_date':'{{data_interval_end | ds}}'
+        }
+    )
+    
+    @task(task_id='python_func2')
+    def python_func2(**kwargs):
+        print(kwargs)
+        print('ti' + kwargs['ti'])
+        print('ds' + kwargs['ds'])
+        print('data_interval_start' + kwargs['data_interval_start'])
+        print('data_interval_end' + kwargs['data_interval_end'])
+        
+    python_t1
+    python_func2()
+    
